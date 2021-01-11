ab7fb07db3fe16d84d46123532899bbff2bb2d10
Osiris Team <59899645+Osiris-Team@users.noreply.github.com>
2021-01-11 03:05:46 +0100

    release 1

--- src/main/java/com/osiris/dyml/utils/UtilsFile.java
+++ src/main/java/com/osiris/dyml/utils/UtilsFile.java
@@ -1,3 +1,11 @@
+/*
+ *  Copyright Osiris Team
+ *  All rights reserved.
+ *
+ *  This software is licensed work.
+ *  Please consult the file "LICENSE" for details.
+ */
+
 package com.osiris.dyml.utils;
 
 import java.io.BufferedReader;
