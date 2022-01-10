fe67817d5f8b0e2dd7727d13b9c223529075fb2f
Osiris Team <osiris_support@pm.me>
2022-01-10 22:53:46 +0100

    added average speeds to benchmark

--- src/test/java/YamlBenchmarks.java
+++ src/test/java/YamlBenchmarks.java
@@ -13,6 +13,8 @@ import org.simpleyaml.configuration.file.YamlFile;
 import org.simpleyaml.exceptions.InvalidConfigurationException;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class YamlBenchmarks {
@@ -21,21 +23,29 @@ public class YamlBenchmarks {
     void compareAll() throws InterruptedException, IOException, InvalidConfigurationException, DuplicateKeyException, DYReaderException, IllegalListException {
         System.out.println("Performing benchmark... Results:");
         System.out.println("Run | Gson | DreamYaml | SnakeYaml | YamlBeans | EoYaml | SimpleYaml");
-        String msGSON;
-        String msDY;
-        String msSNY;
-        String msYB;
-        String msEOY;
-        String msSMY;
+
+        List<Double> resultsGSON = new ArrayList<>();
+        List<Double> resultsDreamYaml = new ArrayList<>();
+        List<Double> resultsSnakeYaml = new ArrayList<>();
+        List<Double> resultsYamlBeans = new ArrayList<>();
+        List<Double> resultsEOYaml = new ArrayList<>();
+        List<Double> resultsSimpleYaml = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             File file = new File(System.getProperty("user.dir")+"/benchmark-small-config.yml");
             File json = new File(System.getProperty("user.dir")+"/benchmark-small-config.json");
             UtilsTimeStopper timer = new UtilsTimeStopper();
+            String msGSON;
+            String msDY;
+            String msSNY;
+            String msYB;
+            String msEOY;
+            String msSMY;
 
             // DREAM-YAML
             timer.start();
             new Gson().fromJson(new FileReader(json), JsonObject.class);
             timer.stop();
+            resultsGSON.add(timer.getMillis());
             msGSON = timer.getFormattedMillis();
 
             // GSON
@@ -45,6 +55,7 @@ public class YamlBenchmarks {
             timer.stop();
             dreamYaml=null;
             msDY = timer.getFormattedMillis();
+            resultsDreamYaml.add(timer.getMillis());
 
             // SNAKE-YAML
             timer.start();
@@ -54,6 +65,7 @@ public class YamlBenchmarks {
             timer.stop();
             snakeYaml=null;
             msSNY = timer.getFormattedMillis();
+            resultsSnakeYaml.add(timer.getMillis());
 
             // YAML-BEANS
             timer.start();
@@ -63,6 +75,7 @@ public class YamlBenchmarks {
             timer.stop();
             yamlBeans=null;
             msYB = timer.getFormattedMillis();
+            resultsYamlBeans.add(timer.getMillis());
 
             // EO-YAML
             timer.start();
@@ -73,6 +86,7 @@ public class YamlBenchmarks {
             timer.stop();
             eolYamlTest=null;
             msEOY = timer.getFormattedMillis();
+            resultsEOYaml.add(timer.getMillis());
 
             // SIMPLE-YAML
             timer.start();
@@ -81,9 +95,26 @@ public class YamlBenchmarks {
             timer.stop();
             simpleYamlTest=null;
             msSMY = timer.getFormattedMillis();
+            resultsSimpleYaml.add(timer.getMillis());
 
             System.out.println("["+i+"] [GSON:"+msGSON+"ms] [DY: "+msDY+"ms] [SNY: "+msSNY+"ms] [YB: "+msYB+"ms] [EOY: "+msEOY+"ms] [SMY: "+msSMY+"ms]");
             Thread.sleep(500);
         }
+        System.out.println("Average read speeds in milliseconds:");
+        System.out.println("Gson: "+calcAverage(resultsGSON));
+        System.out.println("DreamYaml: "+calcAverage(resultsDreamYaml));
+        System.out.println("SnakeYaml: "+calcAverage(resultsSnakeYaml));
+        System.out.println("YamlBeans: "+calcAverage(resultsYamlBeans));
+        System.out.println("EOYaml: "+calcAverage(resultsEOYaml));
+        System.out.println("SimpleYaml: "+calcAverage(resultsSimpleYaml));
+    }
+
+    private Double calcAverage(List<Double> values){
+        Double total = 0.0;
+        for (Double val :
+                values) {
+            total = total + val;
+        }
+        return total / values.size();
     }
 }
